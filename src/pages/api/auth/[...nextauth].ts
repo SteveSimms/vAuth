c5115d6cf9a8fe0d5cfddfacb6939e7a0d1ec98b
steve <simmsstev@gmail.com>
2022-07-29 14:32:39 -0700

    commented out github provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,18 +13,18 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID!,
+    //   clientSecret: process.env.GITHUB_SECRET!,
+    // }),
       
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
-      clientSecret: process.env.GOOGLE_SECRET!
-      ,
+      clientSecret: process.env.GOOGLE_SECRET!,
+      
 
     }),
-      // secret: process.env.NEXTAUTH_SECRET,
+   
     // ...add more providers here
     CredentialsProvider({
       name: "Credentials",
